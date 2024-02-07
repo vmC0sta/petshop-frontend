e0b8a5709ff9b2492f4c61cc2106f7cbcd711d51
vmC0sta <duda.cv2404@gmail.com>
2024-02-07 18:32:22 -0300

    feito metade agendamento

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors:{
@@ -9,6 +9,7 @@ module.exports = {
         amarelo: "var(--amarelo)",
         amarelo_laranja:"var(--amarelo-laranja)",
         texto_preto: "var(--texto-preto)",
+        texto_cinza: "var(--texto-cinza)",
         preto_negrito:"var(--preto-negrito)", 
         botao_cinza: "var(--botao-cinza)",
       },
